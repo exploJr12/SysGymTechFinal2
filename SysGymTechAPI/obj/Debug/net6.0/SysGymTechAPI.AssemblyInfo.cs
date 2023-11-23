898a14006d8c35cd517de2cc6c92a285a3515ce1
exploJr12 <cortezdaniela796@gmail.com>
2023-11-22 19:28:27 -0600

    nose

--- SysGymTechAPI/obj/Debug/net6.0/SysGymTechAPI.AssemblyInfo.cs
+++ SysGymTechAPI/obj/Debug/net6.0/SysGymTechAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysGymTechAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b871c7d6447eeef8a73c911ca6844b39aa2b9653")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysGymTechAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysGymTechAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
