4fe1bee23904bef3689590157a87d3701a064a44
exploJr12 <cortezdaniela796@gmail.com>
2023-11-27 16:56:45 -0600

    dal de details completada

--- SysGymT.AccesoADatos/obj/Debug/net6.0/SysGymT.AccesoADatos.AssemblyInfo.cs
+++ SysGymT.AccesoADatos/obj/Debug/net6.0/SysGymT.AccesoADatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysGymT.AccesoADatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898a14006d8c35cd517de2cc6c92a285a3515ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7277309e879fb178e64f5ef0f5b24438d1bfc77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysGymT.AccesoADatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysGymT.AccesoADatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
