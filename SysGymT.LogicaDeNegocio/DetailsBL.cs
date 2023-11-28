c66f9c88a760696d4666731137bf23fadf64e403
exploJr12 <cortezdaniela796@gmail.com>
2023-11-27 19:20:57 -0600

    bl de details completada

--- SysGymT.LogicaDeNegocio/DetailsBL.cs
+++ SysGymT.LogicaDeNegocio/DetailsBL.cs
@@ -16,25 +16,29 @@ namespace SysGymT.LogicaDeNegocio
         {
             return await DetailsDAL.Create(details);
         }
-
         public async Task<int> ModifyAsync(Details details)
         {
             return await DetailsDAL.Modify(details);
         }
-
         public async Task<int> DeleteAsync(Details details)
         {
             return await DetailsDAL.Delete(details);
         }
-
         public async Task<Details> getByIdAsync(Details details)
         {
             return await DetailsDAL.GetById(details);
         }
-
         public async Task<List<Details>> GetAllDetails(Details details)
         {
             return await DetailsDAL.GetAll();
+        }
+        public async Task<List<Details>> SearchAllAsync(Details pDetails)
+        {
+            return await DetailsDAL.SearchAllDetailsAsync(pDetails);
+        }
+        public static async Task<List<Details>> SearchIncludeProductsAsyn(Details pDetails)
+        {
+            return await DetailsDAL.SearchIncludeProductsAsync(pDetails);
         }
     }
 }
--- SysGymT.LogicaDeNegocio/obj/Debug/net6.0/SysGymT.LogicaDeNegocio.AssemblyInfo.cs
+++ SysGymT.LogicaDeNegocio/obj/Debug/net6.0/SysGymT.LogicaDeNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysGymT.LogicaDeNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898a14006d8c35cd517de2cc6c92a285a3515ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe1bee23904bef3689590157a87d3701a064a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysGymT.LogicaDeNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysGymT.LogicaDeNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
