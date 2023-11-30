4c67c05d5ba9dc70219f2840f46626cf67da4bfb
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2023-11-30 14:02:52 -0600

    change

---
+++ SysGymT.UI.AppWebAspCore/bin/Release/net6.0/publish/CPP/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var trigger = document.querySelector(".hamburger");
+    var overlay = document.querySelector(".overlay");
+    var isClosed = false;
+
+    trigger.addEventListener("click", function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+        if (isClosed == true) {
+            overlay.style.display = "none";
+            trigger.classList.remove("is-open");
+            trigger.classList.add("is-closed");
+            isClosed = false;
+        } else {
+            overlay.style.display = "block";
+            trigger.classList.remove("is-closed");
+            trigger.classList.add("is-open");
+            isClosed = true;
+        }
+    }
+
+    document.querySelector('[data-toggle="offcanvas"]').addEventListener("click", function () {
+        document.getElementById("wrapper").classList.toggle("toggled");
+    });
+});
